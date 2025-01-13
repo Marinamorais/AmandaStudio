4b01ff6be92233af65d5df3cc1c288d4339e0f54
Marinamorais <Marinamorais.564@gmail.com>
2025-01-13 18:16:56 -0300

    Adiciona página de Sobrancelha com layout de cards e botão para retornar aos procedimentos

--- app/sobrancelha/page.jsx
+++ app/sobrancelha/page.jsx
@@ -1,11 +1,51 @@
+"use client";
 import styles from "./sobrancelha.module.css";
 
 
 export default function Sombrancelha() {
+  const Sombrancelha = [
+    {
+      img: "../assets/img/Volume Híbrido.jpg",
+      title: "Sem Henna",
+      description: "Design + Coloração é uma combinação de técnicas de design de sobrancelha e coloração para criar composições visuais impactantes e esteticamente agradáveis.",
+      price: "R$ 35,00",
+    },
+    {
+      img: "../assets/img/LashLifting.jpg",
+      title: "Com rena",
+      description: "O lash lifting é um procedimento que curva os cílios naturais com produtos químicos, proporcionando um olhar mais aberto e expressivo sem a necessidade de extensões.",
+      price: "R$ 50,00",
+    },
+    {
+      img: "../assets/img/Volume Egípcio.jpg",
+      title: "Desing + buço",
+      description: "Designer de sobrancelha, coloração e buço em apenas um pacote. Ideal para quem deseja realçar o olhar e a expressão facial com um visual harmonioso e equilibrado.",
+      price: "R$ 45,00",
+    },
+
+  ];
+  const handleButtonClick = () => {
+    window.location.href = "/procedimentos"; // Substitua com a URL real da página de procedimentos
+  };
+
   return (
-    <div className={styles.sobre}>
-      <h1>Sombrancelha</h1>
-      <p>Este é o conteúdo da página Agenda.</p>
+    <div className={styles.fundo}>
+      <div className={styles.cardContainer}>
+        {Sombrancelha.map((item, idx) => (
+          <div key={idx} className={styles.card}>
+            <img src={item.img} alt={item.title} className={styles.cardImage} />
+            <h3 className={styles.cardTitle}>{item.title}</h3>
+            <p className={styles.cardDescription}>{item.description}</p>
+            <p className={styles.cardPrice}>{item.price}</p>
+          </div>
+        ))}
+        <div className={styles.buttomContainer}>
+          <button className={styles.buttom} onClick={handleButtonClick}>
+            Voltar para procedimentos
+          </button>
+        </div>
+      </div>
     </div>
   );
+
 }

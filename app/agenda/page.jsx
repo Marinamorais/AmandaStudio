dc66b834bb1ff2ce9f35e7eb4df8d8116c9f9a4f
Marinamorais <Marinamorais.564@gmail.com>
2025-01-13 19:06:35 -0300

    Adiciona estilos e estrutura à página de Agenda; implementa layout responsivo e exibe horários de funcionamento

--- app/agenda/page.jsx
+++ app/agenda/page.jsx
@@ -1,12 +1,22 @@
+"use client";
 import styles from "./agenda.module.css";
 import Header from "../components/header/Header";
+import Button from "../components/button/Button"; // Corrigi o nome do componente para Button
 
 export default function Agenda() {
   return (
-    <div className={styles.sobre}>
-      <Header />
-      <h1>Sobre</h1>
-      <p>Este é o conteúdo da página Agenda.</p>
+    <div className={styles.sobre}> {/* Aqui você pode adicionar uma classe ao container principal */}
+      <Header /> {/* Componente Header */}
+      <h1 className={styles.title}>Horário de Funcionamento</h1>
+      <div className={styles.horarios}>
+        <p>Segunda-feira: 09:00 às 17:00</p>
+        <p>Terça-feira: 09:00 às 18:00</p>
+        <p>Quarta-feira: 08:00 às 11:00</p>
+        <p>Quinta-feira: 09:00 às 18:00</p>
+        <p>Sexta-feira: 09:00 às 18:00</p>
+        <p>Sábado: 09:00 às 13:00</p>
+      </div>
+      <Button /> {/* Componente Button */}
     </div>
   );
 }
--- app/components/button/Button.jsx
+++ app/components/button/Button.jsx
@@ -5,7 +5,7 @@ function Contato() {
     <div className={styles.contato}>
       <button 
         className={styles.botaoZap}
-        onClick={() => window.open('https://wa.me/55199991364604', '_blank')}
+        onClick={() => window.open('https://wa.me/c/5519993324124', '_blank')}
       >
         <FaWhatsapp style={{ marginRight: '10px' }} /> Falar no WhatsApp
       </button>

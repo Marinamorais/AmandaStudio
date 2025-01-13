d08371be426d331e514ce8f0daecf68ec4e8ab2b
Marinamorais <Marinamorais.564@gmail.com>
2025-01-13 18:16:45 -0300

    Adiciona página de Buço com layout de cards e botão para retornar aos procedimentos

--- app/buco/page.jsx
+++ app/buco/page.jsx
@@ -1,11 +1,41 @@
+"use client";
 import styles from "./buco.module.css";
 
 
 export default function Buco() {
+  const Buco = [
+    {
+      img: "../assets/img/Volume Híbrido.jpg",
+      title: "Buço avulso",
+      description: "Buço Avulso: deixe seu rosto ainda mais bonito e sem pelos indesejados.",
+      price: "R$ 20,00",
+    },
+
+  ];
+
+  const handleButtonClick = () => {
+    window.location.href = "/procedimentos"; // Substitua com a URL real da página de procedimentos
+  };
+
+
   return (
-    <div className={styles.sobre}>
-      <h1>Buço</h1>
-      <p>Este é o conteúdo da página Agenda.</p>
+    <div className={styles.fundo}>
+      <div className={styles.cardContainer}>
+        {Buco.map((item, idx) => (
+          <div key={idx} className={styles.card}>
+            <img src={item.img} alt={item.title} className={styles.cardImage} />
+            <h3 className={styles.cardTitle}>{item.title}</h3>
+            <p className={styles.cardDescription}>{item.description}</p>
+            <p className={styles.cardPrice}>{item.price}</p>
+          </div>
+        ))}
+        <div className={styles.buttomContainer}>
+          <button className={styles.buttom} onClick={handleButtonClick}>
+            Voltar para procedimentos
+          </button>
+        </div>
+      </div>
     </div>
   );
+
 }
